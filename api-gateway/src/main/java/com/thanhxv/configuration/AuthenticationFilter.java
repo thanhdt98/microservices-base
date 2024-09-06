63e6b0a7de3598bb8cac2ea7f5cb31715fb1642f
thanhdt98 <thanhxv14.doaz@gmail.com>
2024-09-06 22:12:23 +0700

    authentication on api gateway

---
+++ api-gateway/src/main/java/com/thanhxv/configuration/AuthenticationFilter.java
@@ -0,0 +1,87 @@
+package com.thanhxv.configuration;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thanhxv.dto.response.ApiResponse;
+import com.thanhxv.service.IdentityService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Log4j2
+public class AuthenticationFilter implements GlobalFilter, Ordered {
+    IdentityService identityService;
+    ObjectMapper objectMapper;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Enter authentication filter ....");
+        // Get token from authorization header
+        List<String> authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION);
+        if (CollectionUtils.isEmpty(authHeader)) {
+            return unauthenticated(exchange.getResponse());
+        }
+
+        // Verify token
+        String token = authHeader.getFirst().replace("Bearer ", "");
+        log.info("Token {}", token);
+
+//        identityService.introspect(token).subscribe(introspectResponseApiResponse -> {
+//            log.info("introspectResponse {}", introspectResponseApiResponse.getResult().isValid());
+//        });
+
+        return identityService.introspect(token).flatMap(introspectResponseApiResponse -> {
+            if (introspectResponseApiResponse.getResult().isValid()) {
+                return chain.filter(exchange);
+            } else {
+                return unauthenticated(exchange.getResponse());
+            }
+        }).onErrorResume(throwable -> unauthenticated(exchange.getResponse()));
+    }
+
+    /**
+     * explain order de sap xep thu tu cua filter. so cang nho thu tu cang lon
+     * cac filter cua Cloud Gateway dang > 0 => de -1 thi order se cao hon dam bao dc chay truoc
+     * @return
+     */
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+
+    Mono<Void> unauthenticated(ServerHttpResponse response) {
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                .code(1401)
+                .message("Unauthenticated")
+                .build();
+
+        String body = null;
+        try {
+            body = objectMapper.writeValueAsString(apiResponse);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+
+        return response.writeWith(
+                Mono.just(response.bufferFactory().wrap(body.getBytes())));
+    }
+}
