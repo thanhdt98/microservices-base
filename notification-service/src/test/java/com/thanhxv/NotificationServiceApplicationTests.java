47a166e878e768ff461fc19d2137fa48cbade56f
thanhdt98 <thanhxv14.doaz@gmail.com>
2024-09-14 00:33:19 +0700

    initial notification service

---
+++ notification-service/src/test/java/com/thanhxv/NotificationServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thanhxv;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+@Log4j2
+class IdentityServiceApplicationTests {
+
+    //	@Test
+    //	void contextLoads() {
+    //	}
+    //
+
+}
